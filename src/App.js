599a9e8191651dba510b605db455ebefdf72d3b3
Jenny Poon <jenny.poon@live.ca>
2019-01-19 14:18:05 -0800

    added tweets component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from "./components/Header.jsx";
+import Tweets from "./components/Tweets.jsx";
 
 import './styling/App.scss';
 
---
+++ src/components/Tweets.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Tweets extends Component {
+
+  render() {
+
+    return (
+      <div className="header">
+
+      </div>
+    )
+  }
+
+}
+
+export default Tweets;
