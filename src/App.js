13cb7fea82fd7e85001b292ae8ac0285431e3824
Jenny Poon <jenny.poon@live.ca>
2019-01-19 14:16:57 -0800

    added set up for header and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from "./components/Header.jsx";
+
 import './styling/App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Header />
+      <Tweets />
     );
   }
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Siteicon from '../default_avatar.png'
 
 class Header extends Component {
 
   render() {
 
     return (
-
+      <div className="header">
+        <img className="logo_icon" src={Siteicon} alt="bird logo"/>
+        <h1>Twittr</h1>
+      </div>
     )
   }
 
