0b82d0c556b5490d0cdf24c131d2515e877bcda6
Jenny Poon <jenny.poon@live.ca>
2019-01-19 14:43:58 -0800

    reconfig package json and folder structure for proxy

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const env = process.env.NODE_ENV || 'development';
 
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -15,7 +14,6 @@ const knex = require('knex')(knexConfig[env]);
 
 //-----------------//
 
-app.use(logger('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
