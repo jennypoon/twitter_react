8a558d83eb1b062cf5cbeec85c65cb998323d0a8
Jenny Poon <jenny.poon@live.ca>
2019-01-19 13:38:25 -0800

    added basic express set up and cleaned up template

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const env = process.env.NODE_ENV || 'development';
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+//DATABASE
+const knexConfig = require('./knexfile');
+const knex = require('knex')(knexConfig[env]);
+
+//-----------------//
+
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port);
+
+console.log(`App listening on ${port}`);
