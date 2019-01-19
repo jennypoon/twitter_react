31df3059e57b39d98e8e9a44eb4650138fb2bfc3
Jenny Poon <jenny.poon@live.ca>
2019-01-19 14:53:59 -0800

    added get request axios

--- client/src/components/Tweets.jsx
+++ client/src/components/Tweets.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import TweetCard from './TweetCard.jsx'
+
 
 class Tweets extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tweets: []
+    }
+
+    this.getList = this.getList.bind(this)
+  }
+
+  getList() {
+    axios.get("/api/tweets")
+      .then((res) => {
+        this.setState({ tweets: res.data})
+      })
+  }
+
+  componentDidMount() {
+    this.getList();
+  }
 
   render() {
-
+    console.log(this.state)
     return (
       <div className="tweetsContainer">
-
+        <TweetCard />
       </div>
     )
   }
