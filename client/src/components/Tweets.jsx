59c08593b5972eb00afe3de569a657ab99088311
Jenny Poon <jenny.poon@live.ca>
2019-01-19 16:30:50 -0800

    added div for random text on left hand side

--- client/src/components/Tweets.jsx
+++ client/src/components/Tweets.jsx
@@ -48,8 +48,14 @@ class Tweets extends Component {
 
   render() {
     return (
-      <div className="tweetsContainer">
-        {this.displayTweets(this.state.tweets)}
+      <div>
+        <div className="leftContainer">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+        <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div className="tweetsContainer">
+          {this.displayTweets(this.state.tweets)}
+        </div>
       </div>
     )
   }
