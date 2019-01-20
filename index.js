5f1768e15ced14d91bdc1bcda32b87e3adf2a413
Jenny Poon <jenny.poon@live.ca>
2019-01-19 16:22:36 -0800

    post request to update like

--- index.js
+++ index.js
@@ -14,22 +14,37 @@ const knex = require('knex')(knexConfig[env]);
 
 //-----------------//
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-//QUERY FOR RSVP
+//QUERY FOR TWEETS
 app.get('/api/tweets', (req,res) => {
   knex('tweets')
     .select('*')
+    .orderBy('id')
     .asCallback((err, data) => {
-      console.log("QUERY DONE")
-      console.log(data)
       if (err) throw err;
       res.json(data);
+    });
+});
+
+//UPDATE LIKE STATUS OF TWEETS
+app.post('/api/likeTweet', (req,res) => {
+  let likeStatus = parseInt(req.body.tweetlike)
+  knex('tweets')
+    .where('id', req.body.tweetid)
+    .update({'like': likeStatus})
+    .asCallback((err, result) => {
+      knex('tweets')
+        .select('*')
+        .asCallback((err, data) => {
+          if (err) throw err;
+          res.json(data);
+        });
+      if (err) throw err;
     });
 });
 
